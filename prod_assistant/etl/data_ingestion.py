6b19a9642bb47037d4a0d30d231264374e415da1
VENKATA SIVA PRAVEEN <sivapraveen6831@gmail.com>
2025-09-06 00:25:17 +0530

    data ingestion and data scrapper skeletion is crreated

--- prod_assistant/etl/data_ingestion.py
+++ prod_assistant/etl/data_ingestion.py
@@ -7,4 +7,34 @@ from prod_assistant.utilis.model_loader import ModelLoader
 from prod_assistant.utilis.config_loader import load_config
 
 class DataIngestion:
-    
+    """class to handle data transformation and ingestion"""
+    def __init__(self):
+        if os.getenv("ENV", "local").lower() != "production":
+            load_dotenv()
+            log.info("Running in LOCAL mode: .env loaded")
+        else:
+            log.info("Running in PRODUCTION mode")
+
+        self.api_key_mgr = ApiKeyManager()
+        self.config = load_config()
+        log.info("YAML config loaded", config_keys=list(self.config.keys()))
+
+
+    def_load_env_variable(self):
+        pass
+
+    def _get_csv_path(self):
+        pass
+
+    def _load_csv(self):
+        pass
+
+    def transform_data(self):
+        pass
+
+    def store_in_vector_db(self):
+        pass
+
+    def run_pipeline(self):
+        pass
+   
