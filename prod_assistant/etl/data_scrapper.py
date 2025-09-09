5b05d42c94423ce1ee3d23280b03147e7e232202
VENKATA SIVA PRAVEEN <sivapraveen6831@gmail.com>
2025-09-09 23:01:23 +0530

    all files updated

--- prod_assistant/etl/data_scrapper.py
+++ prod_assistant/etl/data_scrapper.py
@@ -4,9 +4,8 @@ import re
 import os
 from bs4 import BeautifulSoup
 import undetected_chromedriver as uc
-from selenium_chromedriver as uc
 from selenium.webdriver.common.by import By
-from selenium .webdriver.common.keys import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
 class FlipkartScrapper:
