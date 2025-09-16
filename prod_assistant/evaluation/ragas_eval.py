0ef22aa48a86188dc07a1fa313c0379b4cdb7e08
VENKATA SIVA PRAVEEN <sivapraveen6831@gmail.com>
2025-09-17 00:13:44 +0530

    evaluation testing added

--- prod_assistant/evaluation/ragas_eval.py
+++ prod_assistant/evaluation/ragas_eval.py
@@ -9,15 +9,42 @@ grpc_aio.init_grpc_aio()
 model_loader=ModelLoader()
 
 
-def evaluate_context_precision():
-    """
-    Evaluate the context precision of the LLM.
-    """
-    pass
+def evaluate_context_precision(query, response, retrieved_context):
+    try:
+        sample = SingleTurnSample(
+            user_input=query,
+            response=response,
+            retrieved_contexts=retrieved_context,
+        )
 
+        async def main():
+            llm = model_loader.load_llm()
+            evaluator_llm = LangchainLLMWrapper(llm)
+            context_precision = LLMContextPrecisionWithoutReference(llm=evaluator_llm)
+            result = await context_precision.single_turn_ascore(sample)
+            return result
 
-def evaluate_response_relevancy():
-    """
-    Evaluate the response relevancy of the LLM.
-    """
-    pass
+        return asyncio.run(main())
+    except Exception as e:
+        return e
+
+def evaluate_response_relevancy(query, response, retrieved_context):
+    try:
+        sample = SingleTurnSample(
+            user_input=query,
+            response=response,
+            retrieved_contexts=retrieved_context,
+        )
+
+        async def main():
+            llm = model_loader.load_llm()
+            evaluator_llm = LangchainLLMWrapper(llm)
+            embedding_model = model_loader.load_embeddings()
+            evaluator_embeddings = LangchainEmbeddingsWrapper(embedding_model)
+            scorer = ResponseRelevancy(llm=evaluator_llm, embeddings=evaluator_embeddings)
+            result = await scorer.single_turn_ascore(sample)
+            return result
+
+        return asyncio.run(main())
+    except Exception as e:
+        return e
