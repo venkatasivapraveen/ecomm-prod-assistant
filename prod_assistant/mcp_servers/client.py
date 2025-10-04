b8a46c7196f4c4654381c418fd2555f9f9fc10ab
VENKATA SIVA PRAVEEN <sivapraveen6831@gmail.com>
2025-10-05 01:27:17 +0530

    mcp and agentic flow added

--- prod_assistant/mcp_servers/client.py
+++ prod_assistant/mcp_servers/client.py
@@ -6,7 +6,7 @@ async def main():
         "hybrid_search": {   # server name
             "command": "python",
             "args": [
-                r"D:\complete_content_new\llmops-batch\ecomm-prod-assistant\prod_assistant\mcp_servers\product_search_server.py"
+                r"/Users/venkatasivapraveenpallaprolu/ecomm-prod-assistant/prod_assistant/mcp_servers/product_search_server.py"
             ],  # absolute path
             "transport": "stdio",
         }
